2daf83927d15f79b7e17af935c9aa29b2f1b625c
GuilhemeBorin <guilherme.galeno@sptech.school>
2024-11-07 18:46:20 -0300

    sobindo atividade

--- src/main/java/school/sptech/Imagem.java
+++ src/main/java/school/sptech/Imagem.java
@@ -1,4 +1,49 @@
 package school.sptech;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Imagem {
+    private List<Figura> figuras = new ArrayList<>();
+
+    public Imagem() {}
+
+    public void adicionar(Figura figura) {
+        figuras.add(figura);
+    }
+
+    public Double calcularSomaDasAreas() {
+        Double somaDasAreas = 0.0;
+        for (Figura figura : figuras) {
+            somaDasAreas += figura.calcularArea();
+        }
+        return somaDasAreas;
+    }
+
+    public List<Figura> buscarPorAreaMaiorQue20() {
+        List<Figura> areasMaioresQueVinte = new ArrayList<>();
+        for (Figura figura : figuras) {
+            if (figura.calcularArea() > 20) {
+                areasMaioresQueVinte.add(figura);
+            }}
+        return areasMaioresQueVinte;
+    }
+
+    public List<Figura> buscarQuadrados() {
+        List<Figura> ApenasQuadrados = new ArrayList<>();
+        for(Figura figura : figuras) {
+            if (figura instanceof Quadrado)
+                ApenasQuadrados.add(figura);
+        }
+        return ApenasQuadrados;
+    }
+
+    public List<Figura> getFiguras() {
+        return figuras;
+    }
+
+    public void setFiguras(List<Figura> figuras) {
+        this.figuras = figuras;
+    }
+
 }
